c999a1bf0b7e40d44d24c5b96e62705e3c3b8e37
magmom7 <84061081+magmom7@users.noreply.github.com>
2021-06-19 12:44:21 +0900

    Add files via upload

---
+++ stack/10828.py
@@ -0,0 +1,44 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+stack = []
+
+
+def push(x):
+    stack.append(x)
+
+
+def pop():
+    if(not stack):
+        return -1
+    else:
+        return stack.pop()
+
+
+def size():
+    return len(stack)
+
+
+def empty():
+    return 0 if stack else 1
+
+
+def top():
+    return stack[-1] if stack else -1
+
+
+for _ in range(N):
+    input_split = sys.stdin.readline().rstrip().split()
+
+    order = input_split[0]
+
+    if order == "push":
+        push(input_split[1])
+    elif order == "pop":
+        print(pop())
+    elif order == "size":
+        print(size())
+    elif order == "empty":
+        print(empty())
+    elif order == "top":
+        print(top())
