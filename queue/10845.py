e1be2fc71dda1a6af00cd791a0fc783613356c16
magmom7 <84061081+magmom7@users.noreply.github.com>
2021-06-22 17:43:04 +0900

    Add files via upload

---
+++ queue/10845.py
@@ -0,0 +1,50 @@
+import sys
+
+n = int(input())
+lst = []
+
+def push(x):
+    lst.append(x)
+
+def pop():
+    if len(lst) == 0:
+        print(-1)
+    else:
+        print(lst.pop(0))
+
+def size():
+    print(len(lst))
+
+def empty():
+    if len(lst) == 0:
+        print(1)
+    else:
+        print(0)
+
+def front():
+    if len(lst) == 0:
+        print(-1)
+    else:
+        print(lst[0])
+
+def back():
+    if len(lst) == 0:
+        print(-1)
+    else:
+        print(lst[-1])
+
+for i in range(n):
+    a = sys.stdin.readline().strip().split(' ')
+
+    if a[0] == 'push':
+        push(a[1])
+    elif a[0] == 'pop':
+        pop()
+    elif a[0] == 'size':
+        size()
+    elif a[0] == 'empty':
+        empty()
+    elif a[0] == 'front':
+        front()
+    elif a[0] == 'back':
+        back()
