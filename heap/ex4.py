24b1d85a77cd32063f62f0300da4c0df28eb3949
magmom7 <84061081+magmom7@users.noreply.github.com>
2021-08-05 21:51:49 +0900

    Update ex4.py

--- heap/ex4.py
+++ heap/ex4.py
@@ -10,3 +10,10 @@ class Heap:
         
         returned_data = self.heap_array[1]
         return returned_data
+#heap = Heap(15)
+#heap.insert(10)
+#heap.insert(8)
+#heap.insert(5)
+#heap.insert(4)
+#heap.insert(20)
+#heap.heap_array
