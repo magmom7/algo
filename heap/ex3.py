99e2a6abe3013b3ea5fea48e3d8c3f5f8ee6db64
magmom7 <84061081+magmom7@users.noreply.github.com>
2021-08-05 21:50:00 +0900

    Update ex3.py

--- heap/ex3.py
+++ heap/ex3.py
@@ -30,3 +30,11 @@ class Heap:
             inserted_idx = parent_idx
         
         return True
+    
+#heap = Heap(15)
+#heap.insert(10)
+#heap.insert(8)
+#heap.insert(5)
+#heap.insert(4)
+#heap.insert(20)
+#heap.heap_array
