c19d762550c55811d8cc0c1e0ffcf9faf2f1fa1e
magmom7 <84061081+magmom7@users.noreply.github.com>
2021-08-16 22:58:00 +0900

    Update ex3.py

--- mergesort/ex3.py
+++ mergesort/ex3.py
@@ -22,3 +22,11 @@ def merge(left, right):
         right_point += 1
     
     return merged
+
+def mergesplit(data):
+    if len(data) <= 1:
+        return data
+    medium = int(len(data) / 2)
+    left = mergesplit(data[:medium])
+    right = mergesplit(data[medium:])
+    return merge(left, right)
