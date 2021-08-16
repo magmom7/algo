a3fd79dd8924b080f7e10bec020c43a01f2d1f8c
magmom7 <84061081+magmom7@users.noreply.github.com>
2021-08-16 22:53:58 +0900

    Add files via upload

---
+++ mergesort/ex3.py
@@ -0,0 +1,24 @@
+def merge(left, right):
+    merged = list()
+    left_point, right_point = 0, 0
+    
+    # case1 - left/right 둘다 있을때
+    while len(left) > left_point and len(right) > right_point:
+        if left[left_point] > right[right_point]:
+            merged.append(right[right_point])
+            right_point += 1
+        else:
+            merged.append(left[left_point])
+            left_point += 1
+
+    # case2 - left 데이터가 없을 때
+    while len(left) > left_point:
+        merged.append(left[left_point])
+        left_point += 1
+        
+    # case3 - right 데이터가 없을 때
+    while len(right) > right_point:
+        merged.append(right[right_point])
+        right_point += 1
+    
+    return merged
