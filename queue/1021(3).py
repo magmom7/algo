00ec196c6eeece57334339652fa19006a138b623
magmom7 <84061081+magmom7@users.noreply.github.com>
2021-08-25 22:26:16 +0900

    Update 1021(3).py

--- queue/1021(3).py
+++ queue/1021(3).py
@@ -1,16 +1,23 @@
 from collections import deque
 
-N, M = map(int, input().split())
-li = list(map(int, input().split()))
-queue = deque(range(1, N+1))
-cnt = 0
-for n in li:
-    while queue[0] != n:
-        t = queue.index(n)
-        if t <= len(queue)-t-1:
-            queue.append(queue.popleft())
+n,m = map(int,input().split())
+# deque([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) 이런식으로 큐를 표현한다.
+data = deque([i for i in range(1,n+1)])
+#뽑아내려고 하는 수의 위치가 index변수에 순서대로 담아있다.
+index = list(map(int,input().split()))
+
+count = 0
+for num in index:
+    while 1:
+        if data[0] == num:
+            data.popleft()
+            break
         else:
-            queue.appendleft(queue.pop())
-        cnt += 1
-    queue.popleft()
-print(cnt)
+            if data.index(num) <= len(data)//2:
+                data.rotate(-1)
+                count += 1
+            else:
+                data.rotate(1)
+                count += 1
+
+print(count)
